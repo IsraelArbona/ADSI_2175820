009d9b359afad4d7fd0712083a01e58e1f6832a1
IsraelArbona <i.arbonammisena.edu.co>
2021-06-22 21:36:21 -0500

    Inicio de proyectos adsi virtual 2175820

---
+++ Ejemplos/ejercicio_1.php
@@ -0,0 +1,24 @@
+<?php
+  /*
+  echo "Israel";
+  echo "<br>";
+  print('Israel');
+  */
+
+  // comentar una sola linea de codigo.
+  
+  $titulo = "Introducción array"
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 1</title>
+  </head>
+  <body>
+    <h1><?php print($titulo) ?></h1>
+  </body>
+</html>
